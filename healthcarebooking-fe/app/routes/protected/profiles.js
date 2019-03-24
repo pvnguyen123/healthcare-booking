ffd51a9a30222d58303126724471e2fdb27eaf7b
pvnguyen123 <hpnguyen@hpnguyen-mn2.linkedin.biz>
2019-03-24 12:56:48 -0700

    Add sample chart

--- healthcarebooking-fe/app/routes/protected/profiles.js
+++ healthcarebooking-fe/app/routes/protected/profiles.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    setupController(controller, model) {
+        // Call _super for default behavior
+        this._super(controller, model);
+        // Implement your custom setup after
+        let data = {
+            datasets: [{
+                data: [10, 20, 30],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                ],
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: [
+                'Open',
+                'Completed',
+                'In Progress'
+            ]
+        };
+
+        controller.set('data', data)
+      }
 });
